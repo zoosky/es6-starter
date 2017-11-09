618becf4a649fd36af303af1f7a85cf575cfb084
jofftiquez@gmail.com <jofftiquez@gmail.com>
2017-11-09 11:49:05 +0800

    created a new sample (a better one)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-const x = '123';
-const what = 'ES6';
+/**
+  Write your ES6 shits here, you can 
+  remove the sample codes below.
+  The code below is just an example,
+  do not limit your code just by following
+  the stupid sample below.
+*/
 
-console.log(typeof +x);
-console.log(`I can write ${ what } here.`);
+export default class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  talk () {
+    console.log(`Cats can't talk... or do they?`);
+  }
+
+  meow () {
+    console.log(`${ this.name } says 'meow bitch!'`);
+  }
+
+  walk () {
+    console.log(`${ this.name } is too lazy to walk.`);
+  }
+
+  sleep (n) {
+    console.log(`${ this.name } will wake up in ${ n } seconds.`);
+    setTimeout(() => {
+      console.log(`${ this.name } is now awake! Prepare the food!`);
+    }, n * 1000);
+  }
+
+  feed (food) {
+    if(food === 'fish') {
+      console.log(`${ this.name } is happily eating ${ food }.`);
+      return;
+    }
+    console.log(`${ this.name } doesn't want to eat ${ food }.`);
+  }
+}
