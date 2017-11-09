ccc9e0193532432d4f1fb8b30df221188b08f904
jofftiquez@gmail.com <jofftiquez@gmail.com>
2017-11-09 11:49:23 +0800

    updated tester.js

--- tester.js
+++ tester.js
@@ -1,1 +1,10 @@
-require('./dist');
+const Cat = require('./dist').default;
+
+const cat = new Cat('Kidlat');
+
+cat.talk();
+cat.meow();
+cat.walk();
+cat.sleep(3);
+cat.feed('fish');
+cat.feed('dog food');
