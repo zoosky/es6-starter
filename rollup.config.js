af50ca48d9085d5284c14b1bf05c616617da72f2
Andreas Kapp <zoosky@gmail.com>
2018-04-05 13:59:13 +0200

    babel wip

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,17 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'build/es-starter.js',
     name: 'Cat',
     format: 'umd'
-  }
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    })
+  ]
 };
